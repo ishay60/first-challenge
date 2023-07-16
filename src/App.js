f8e74abd8bdfc3130140c692157b4fdf52036044
Ishay Rosengarten <ishay60@gmail.com>
2023-07-16 14:54:01 +0300

    Profile card without skills css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css";
+import ishayPhoto from "./photos/ishayPhoto.jpg";
 
-function App() {
+// first-challenge\src\photos\ishayPhoto.jpg
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          <SkillList className="skill-list" />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src={ishayPhoto} alt="" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>{ishay.name}</h1>
+      <p>
+        <Description />
+      </p>
+    </div>
+  );
+}
+
+function Description() {
+  return ishay.description.map((description, index) => {
+    return (
+      <div key={index} description={description}>
+        {description}
+      </div>
+    );
+  });
+}
+
+function SkillList() {
+  return ishay.skills.map((skill, index) => {
+    return (
+      <div>
+        <Skill className="skill" key={index} skill={skill} />
+      </div>
+    );
+  });
+}
+const Skill = ({ skill }) => <p>{skill} 💪</p>;
+
+const ishay = {
+  name: "Ishay Rosengarten",
+  photo: ishayPhoto,
+  description: [
+    " Highly passionate about technology and software development.",
+    "Quick learner with a strong ability to adapt to new technologies and programming languages.",
+  ],
+  skills: ["c++", "JS", "React", "Unity/C#", "HTML", "Css"],
+};
